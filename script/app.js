4297c9069db77cca7dcd8bf366dc7f4b88f0a42e
Marcos Willian <marcoswillianl1987@gmail.com>
2024-11-19 12:44:12 -0400

    tabela
    Criando uma tabela dinamicamente usando os dados em um arquivo json

--- script/app.js
+++ script/app.js
@@ -2,33 +2,48 @@ const container = document.getElementById('container');
 const carregando = document.getElementById('carregando');
 const mostrarDados = document.getElementById('mostrar-dados');
 const mensagemDados = document.getElementById('mensagem-dados');
+const url = 'https://jsonplaceholder.typicode.com/posts';
 
-function carregarDados() {
-    mostrarDados.style.display = 'none';
-
-    return new Promise((resolve, reject) => {
-        const sucesso = true;
-        setTimeout(() => {
-            if (sucesso) {
-                resolve('Os dados foram carregados com sucesso');
-            } else {
-                reject('Erro ao carregar os dados');
-            }
-        }, 3000);
-    });
+async function carregarDados() {
+    try {
+        const resposta = await fetch(url);
+        const dados = await resposta.json();
+        if (!resposta.ok) {
+            throw new Error('Erro ao carregar os arquivos');
+        }
+        return dados;
+    }
+    catch (erro) {
+        const msgErro = erro;
+        return msgErro;
+    }
 }
 
-
-async function exibirDados() {
+async function criarTabela() {
     try {
         const resposta = await carregarDados();
         carregando.style.display = 'none';
         mostrarDados.style.display = 'flex'
-        mensagemDados.innerHTML = resposta;
+        console.log(resposta);
+        resposta.forEach(objeto => {
+            const tabelas = document.createElement('table');
+            tabelas.classList.add('tabelas-dados');
+            container.appendChild(tabelas);
+
+            tabelas.forEach((tabela, indice) => {
+                const tbody = document.createElement('tbody');
+                const tr = document.createElement('tr');
+                const th = document.createElement('th');
+                const td = document.createElement('td');
+
+                
+            })
+        })
+        
     }
     catch (erro) {
         console.error(erro);
     }
 }
 
-exibirDados();
+criarTabela();
